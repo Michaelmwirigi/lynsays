a3d127bf97af8ee9d32eab3a54455f503f7e3866
Michael Mwirigi <mwirigimichael@gmail.com>
2016-05-18 18:02:42 +0300

    added theme

---
+++ content/themes/typesome/assets/js/index.js
@@ -0,0 +1,88 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".postcontent_typesome").fitVids();
+
+        function casperFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".postcontent_typesome").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
+
+
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
+
+    });
+
+}(jQuery));
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+(function ($,e,t,n,i) {
+    
+
+    $(document).ready(function(){
+        
+        $('.menu_dropdown')
+          .dropdown({
+            on: 'hover'
+          });
+
+      $('.site-head')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        });
+    });
+
+   
+
+}(jQuery));
+
+
